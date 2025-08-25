d8103692f070f8c86b59d6b2297464f582f8c386
axue <sherrrryz@outlook.com>
2025-08-25 15:52:47 -0400

    change margin

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="flex-auto min-w-0 mt-2 md:mt-6 flex flex-col px-6 sm:px-4 md:px-0 max-w-[624px] w-full">
+          <main className="flex-auto min-w-0 mt-2 md:mt-6 flex flex-col px-6 sm:px-12 md:px-16 max-w-[1600px] w-full">
             <Navbar />
             {children}
             <Footer />
